5a8c3a66f39779b8aaf50470b0784a462aab0f2f
watasuke102 <watasuke102@gmail.com>
2024-12-18 10:03:13 +0900

    update: show status bar on running

--- unity-project/Assets/Session.cs
+++ unity-project/Assets/Session.cs
@@ -16,6 +16,28 @@ public class Session : MonoBehaviour
   {
     NRDebugger.logLevel = LogLevel.Warning;
     Init();
+    // show status bar; from https://discussions.unity.com/t/how-to-not-cover-up-the-android-ios-top-status-bar-eg-with-time-icons-in-mobile-app/863652/8
+    using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+    {
+      using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+      {
+        using (var window = activity.Call<AndroidJavaObject>("getWindow"))
+        {
+          using (var Decor = window.Call<AndroidJavaObject>("getDecorView"))
+          {
+            using (var controller = Decor.Call<AndroidJavaObject>("getWindowInsetsController"))
+            {
+              using (var type = new AndroidJavaClass("android.view.WindowInsets$Type"))
+              {
+                controller.Call("show", type.CallStatic<int>("statusBars"));
+              }
+            }
+          }
+          window.Call("setFlags", 512, 512);
+          window.Call("setStatusBarColor", unchecked((int)0x00005700)); //for transparent status bar
+        }
+      }
+    }
   }
 
   int app_clicked = 0;

7cba2253600721465f0b4eaaf9e82cdde7927d1c
watasuke102 <watasuke102@gmail.com>
2025-01-26 15:54:24 +0900

    update: enable alpha blending

--- plugin/src/client.cpp
+++ plugin/src/client.cpp
@@ -46,6 +46,8 @@ Client::Render(int32_t camera_id)
   glDisable(GL_CULL_FACE);
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LESS);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   this->remote_->Render(camera->get()->Get());
 }
 
